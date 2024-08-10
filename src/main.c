5914b0e8873f3659edd860f00c98cef6c586ab79
Doomowenok <doomowenok@icloud.com>
2024-08-10 22:02:19 +0300

    Check the connection with variables between embedded Lua and C.

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 #include "../lib/lua/src/lualib.h"
 #include "../lib/lua/src/lauxlib.h"
 
-int main(int args, char* arg[])
+void lua_example_dofile(void)
 {
     lua_State* lua = luaL_newstate();
 
@@ -12,6 +12,24 @@ int main(int args, char* arg[])
     luaL_dofile(lua, "./scripts/factorial.lua");
 
     lua_close(lua);
+}
 
+void lua_example_getvar(void)
+{
+    lua_State* lua = luaL_newstate();
+
+    luaL_dostring(lua, "some_var = 486");
+    lua_getglobal(lua, "some_var");
+    lua_Number some_var_in_c = lua_tonumber(lua, -1);
+
+    printf("The value of some_var insise C is: %d\n", (int)some_var_in_c);
+
+    lua_close(lua);
+}
+
+int main(int args, char* arg[])
+{
+    // lua_example_dofile();
+    lua_example_getvar();
     return 0;
 }
