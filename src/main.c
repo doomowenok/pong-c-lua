def12e494471e70810198e83c06568f71531f69b
Doomowenok <doomowenok@icloud.com>
2024-08-10 23:48:56 +0300

    Check how stack works.

--- src/main.c
+++ src/main.c
@@ -27,9 +27,29 @@ void lua_example_getvar(void)
     lua_close(lua);
 }
 
+void lua_example_stack(void)
+{
+    lua_State* lua = luaL_newstate();
+
+    lua_pushnumber(lua, 286);   // stack [1] or stack [-3]
+    lua_pushnumber(lua, 386);   // stack [2] or stack [-2]
+    lua_pushnumber(lua, 486);   // stack [3] or stack [-1]
+
+    lua_Number element;
+    element = lua_tonumber(lua, -1);
+    printf("The last added element in position 3 of the stack is %d\n", (int)element);
+
+    lua_remove(lua, 2);
+    element = lua_tonumber(lua, 2);
+    printf("The last added element in position 2 of the stack is %d\n", (int)element);
+
+    lua_close(lua);
+}
+
 int main(int args, char* arg[])
 {
     // lua_example_dofile();
-    lua_example_getvar();
+    // lua_example_getvar();
+    lua_example_stack();
     return 0;
 }
