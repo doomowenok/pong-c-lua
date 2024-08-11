55f953325ab4ed4563f8de5fb160bb13d800a5a2
Doomowenok <doomowenok@icloud.com>
2024-08-11 17:04:28 +0300

    Fix error in defines.

--- src/main.c
+++ src/main.c
@@ -6,8 +6,8 @@
 
 #define FALSE 0
 #define TRUE 1
-#define WINDOW_WIDTH = 800
-#define WINDOW_HEIGHT = 600
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
 
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
